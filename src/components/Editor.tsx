a6c7ab7e2f44cffc070960fe972369f935b04b3e
Etheryte <karl.tarvas@suse.com>
2021-03-23 13:02:14 +0100

    Refacto scaffolding

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,10 +1,13 @@
 import { editor, languages } from "monaco-editor";
-import MonacoEditor, { EditorDidMount, EditorWillMount } from "react-monaco-editor";
-import { useEffect, useRef, useState } from "react";
+import MonacoEditor, { EditorDidMount, EditorWillMount, monaco } from "react-monaco-editor";
+import { useEffect, useState } from "react";
 
 import { Level } from "../levels";
 
 import "./editor.scss";
+
+type Monaco = typeof monaco;
+type Instance = monaco.editor.IStandaloneCodeEditor;
 
 type Props = {
   level: Level;
@@ -14,53 +17,47 @@ const USER_PATH = "inmemory://user";
 const SYSTEM_PATH = "inmemory://system";
 
 export default (props: Props) => {
+  const [instance, setInstance] = useState<Instance | null>(null);
+  const [controller, setController] = useState<Monaco | null>(null);
+
   // Levels start out in unsolved state
   const [markers, setMarkers] = useState<monaco.editor.IMarker[]>([]);
 
   // When we receive a new level, reset the state
   useEffect(() => {
-    // TODO: Reset
-  }, [props.level]);
+    // If the editor is not ready yet, just ignore and go when it is ready
+    if (!instance || !controller) {
+      return;
+    }
 
-  const onBeforeMount: EditorWillMount = (monaco) => {
-    const defaults = monaco.languages.typescript.typescriptDefaults;
-    const baseOptions = defaults.getCompilerOptions();
-    const options: languages.typescript.CompilerOptions = {
-      // The compiler breaks in the browser without `allowNonTsExtensions: true` ¯\_(ツ)_/¯
-      ...baseOptions,
-      strict: true,
-      // Remove default browser interface definitions etc so we don't get a bunch of unrelated things in autocomplete
-      noLib: true,
-    };
-    defaults.setCompilerOptions(options);
-
-    // TODO: Update when level changes
-    // Could possibly set extra context and stuff like this
-    defaults.setExtraLibs([
-      {
-        content: props.level.context,
-      },
-    ]);
-  };
-
-  const onMount: EditorDidMount = (instance, monaco) => {
-    // This is only required to keep hot reload happy-ish
-    for (const model of monaco.editor.getModels()) {
+    // This is required both to keep hot reload happy-ish and to avoid leaks on level changes
+    for (const model of controller.editor.getModels()) {
       model.dispose();
     }
     instance.setModel(null);
 
-    monaco.editor.onDidChangeMarkers((uris) => {
+    controller.editor.onDidChangeMarkers((uris) => {
       for (const uri of uris) {
-        const markers = monaco.editor.getModelMarkers({
+        const markers = controller.editor.getModelMarkers({
           resource: uri,
         });
         console.log("markers for " + uri.toString(), markers);
+        // TODO: Set system content once there's no markers for user
+        // TODO: Validate level based on system markers
       }
     });
 
-    const userModel = editor.createModel(props.level.text, "typescript", monaco.Uri.parse(USER_PATH));
-    const systemModel = editor.createModel("", "typescript", monaco.Uri.parse(SYSTEM_PATH));
+    const defaults = controller.languages.typescript.typescriptDefaults;
+    // TODO: Update when level changes
+    // Could possibly set extra context and stuff like this
+    defaults.setExtraLibs([
+      {
+        content: props.level.context ?? "",
+      },
+    ]);
+
+    const userModel = editor.createModel(props.level.text, "typescript", controller.Uri.parse(USER_PATH));
+    const systemModel = editor.createModel("", "typescript", controller.Uri.parse(SYSTEM_PATH));
     instance.setModel(userModel);
     console.log(editor.getModels());
 
@@ -72,19 +69,24 @@ export default (props: Props) => {
     // TODO: If the level text has a selection, highlight it
     // editor.setSelection(new Selection(5, 16, 5, 19));
     // TODO: We currently load all languages, perhaps we can remove this?
-    // const typescriptDefaults = monaco.languages.typescript.typescriptDefaults;
-    // TODO: getCompilerOptions ??
-    // No clue why this API is this way
-    /*
-    const getWorker = await monaco.languages.typescript.getTypeScriptWorker();
-    const tempModel = monaco.editor.createModel("const foo: string = 32", "typescript");
-    console.log("model", tempModel);
-    const worker = await getWorker(tempModel.uri);
-    console.log("worker", worker);
-    const result = await worker.getEmitOutput(tempModel.uri.toString());
-    console.log(result);
-    console.log(await worker.getSyntacticDiagnostics(tempModel.uri.toString()));
-    */
+  }, [instance, controller, props.level]);
+
+  const onBeforeMount: EditorWillMount = (controller: Monaco) => {
+    const defaults = controller.languages.typescript.typescriptDefaults;
+    const baseOptions = defaults.getCompilerOptions();
+    const options: languages.typescript.CompilerOptions = {
+      // The compiler breaks in the browser without `allowNonTsExtensions: true` ¯\_(ツ)_/¯
+      ...baseOptions,
+      strict: true,
+      // Remove default browser interface definitions etc so we don't get a bunch of unrelated things in autocomplete
+      noLib: true,
+    };
+    defaults.setCompilerOptions(options);
+  };
+
+  const onMount: EditorDidMount = (instance: Instance, monaco: Monaco) => {
+    setInstance(instance);
+    setController(monaco);
   };
 
   const options: monaco.editor.IStandaloneEditorConstructionOptions = {
@@ -101,6 +103,7 @@ export default (props: Props) => {
     inlineHints: {
       enabled: false,
     },
+    language: "typescript",
     // We set a model in `onMount()`
     model: null,
   };
@@ -109,12 +112,7 @@ export default (props: Props) => {
     <>
       <pre>{JSON.stringify(markers)}</pre>
       <div className="editor-area">
-        <MonacoEditor
-          options={options}
-          editorWillMount={onBeforeMount}
-          editorDidMount={onMount}
-          language="typescript"
-        />
+        <MonacoEditor options={options} editorWillMount={onBeforeMount} editorDidMount={onMount} />
       </div>
     </>
   );
--- src/levels/1.tsx
+++ src/levels/1.tsx
@@ -9,6 +9,8 @@ export const text = `/**
 type JumpOver = 
 `;
 
+// TODO: How to figure out where to start validation?
+// Could intentionally create a greppable error a-la `const Marker: never = 42;`
 export const validateText = `const validate: (JumpOver extends any ? any : never) = true;`;
 
 export const validate = (markers: monaco.editor.IMarker[]) => {
