6a09b1ee5801c676aed271958520b21a43666531
Etheryte <karl.tarvas@suse.com>
2021-03-27 20:36:59 +0100

    Initial level setup

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,3 +1,16 @@
+import "./game.scss";
+
 export default () => {
-  return <div />;
+  return (
+    <div className="game">
+      <div className="top-fill" />
+      <div className="clouds-high" />
+      <div className="clouds-low" />
+      <div className="hills" />
+      <div className="ground-top">
+        <div className="character" />
+      </div>
+      <div className="ground-fill" />
+    </div>
+  );
 };
