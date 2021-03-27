80afcae2302d630e69165dc6ec95b8678e4a4c39
Etheryte <karl.tarvas@suse.com>
2021-03-27 20:57:21 +0100

    WIP graphics

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -3,12 +3,13 @@ import "./game.scss";
 export default () => {
   return (
     <div className="game">
-      <div className="top-fill" />
+      <div className="top-edge" />
       <div className="clouds-high" />
       <div className="clouds-low" />
       <div className="hills" />
       <div className="ground-top">
         <div className="character" />
+        <div className="obstacle" />
       </div>
       <div className="ground-fill" />
     </div>
